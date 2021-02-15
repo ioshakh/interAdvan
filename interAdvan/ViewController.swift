1cfdb60cbe23ec5518c7dec4c75750f8a3284563
Shahzod <bektemirovshahzodbek@gmail.com>
2021-02-15 22:09:22 +0500

    Initial Commit

---
+++ interAdvan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  interAdvan
+//
+//  Created by shahzod on 2/15/21.
+//  Copyright © 2021 shahzod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
